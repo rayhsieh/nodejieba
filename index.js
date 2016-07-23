7690915401b51c0c0963b6c101dedcbd240bb1fa
Ray Hsieh <ray@sentri.me>
2016-07-23 09:54:52 +0800

    explicitly declare variable for running in strict mode.

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ nodejieba.DEFAULT_STOP_WORD_DICT = __dirname + "/dict/stop_words.utf8";
 var isDictLoaded = false;
 
 var someFunct = nodejieba.load;
-nodejieba.load = function (dictJson) {
+nodejieba.load = function(dictJson) {
   if (!dictJson) {
     dictJson = {}
   }
-  dict         = dictJson.dict       || nodejieba.DEFAULT_DICT;
-  hmmDict      = dictJson.hmmDict    || nodejieba.DEFAULT_HMM_DICT;
-  userDict     = dictJson.userDict   || nodejieba.DEFAULT_USER_DICT;
-  idfDict      = dictJson.idfDict    || nodejieba.DEFAULT_IDF_DICT;
-  stopWordDict = dictJson.stopWordDict || nodejieba.DEFAULT_STOP_WORD_DICT;
+  var dict = dictJson.dict || nodejieba.DEFAULT_DICT;
+  var hmmDict = dictJson.hmmDict || nodejieba.DEFAULT_HMM_DICT;
+  var userDict = dictJson.userDict || nodejieba.DEFAULT_USER_DICT;
+  var idfDict = dictJson.idfDict || nodejieba.DEFAULT_IDF_DICT;
+  var stopWordDict = dictJson.stopWordDict || nodejieba.DEFAULT_STOP_WORD_DICT;
 
   isDictLoaded = true;
   return someFunct.call(this, dict, hmmDict, userDict, idfDict, stopWordDict);
@@ -24,7 +24,7 @@ nodejieba.load = function (dictJson) {
 
 function wrapWithDictLoad(obj, functName) {
   var someFunct = obj[functName];
-  obj[functName] = function () {
+  obj[functName] = function() {
     if (!isDictLoaded) {
       nodejieba.load();
     }
@@ -42,4 +42,3 @@ wrapWithDictLoad(nodejieba, "extract");
 wrapWithDictLoad(nodejieba, "insertWord");
 
 module.exports = nodejieba;
-
